08e2a4b4529f7c061d52ca20da2ea391428bad76
Kevin Kenyon <kwkenyon@us.ibm.com>
2020-11-27 10:27:34 -0600

    Add temporal difference learning + simulation code

--- tictactoe.py
+++ tictactoe.py
@@ -1,14 +1,17 @@
 import pickle
+import random
 
 STATES_FILE = './data/states.dat'
 VALUE_FILE = './data/value.pickle'
 
 class TicTacToe(object):
 
-    def __init__(self, p1, p2):
+    def __init__(self, p1, p2, alpha=.9, epsilon=.1):
         self.board = Board()
         self.p1 = p1
         self.p2 = p2
+        self.epsilon = epsilon
+        self.alpha = alpha
 
     def get_input(self, player):
         while True:
@@ -29,6 +32,7 @@ class TicTacToe(object):
             if self.board.move(row, col, player):
                 return
             print('Input is invalid. Try again.')
+
 
     def score(self):
         p1_token = str(self.p1)
@@ -110,10 +114,89 @@ class TicTacToe(object):
                 value_table = pickle.load(handle)
         return value_table
 
+    def saveValueTable(self):
+        with open(VALUE_FILE, 'wb') as handle:
+            pickle.dump(self.value_table, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
     def initSimulation(self, new=True):
         self.value_table = self.initValueFunction(new)
-        print(self.value_table)
+        self.board = Board()
 
+    def simulate(self, new=True, num_games=2):
+        self.initSimulation(new)
+
+        score = self.score()
+        print(self.board)
+        print(f'Score: {score}')
+
+        wins = 0
+        losses = 0
+        draws = 0
+
+        for game in range(num_games):
+            print(f'Simulating Game {game + 1}')
+            while score == 0 and self.board.has_move() :
+                # Player X
+                actions = self.board.get_actions()
+
+                st = self.board.get_board_state_str()
+                value_st = self.value_table[st]
+
+                max_value = 0
+                next_action = None
+
+                is_greedy = random.random() > self.epsilon
+
+                if is_greedy:
+                    print('Greedy action.')
+                    for action in actions:
+                        board_state_str = self.board.get_board_state_after_action(action, p1)
+                        value_s = self.value_table[board_state_str]
+                        if value_s > max_value:
+                            max_value = value_s
+                            next_action = action
+                    if not next_action:
+                        next_action = random.randint(0, len(actions) - 1)
+
+                    board_state_str = self.board.get_board_state_after_action(next_action, p1)
+                    value_st1 = self.value_table[board_state_str]
+                    new_value_st = value_st + self.alpha * (value_st1 - value_st)
+                    self.value_table[st] = new_value_st
+                else:
+                    print('Random action.')
+                    # Pick randomly between all actions
+                    next_action = random.randint(0, len(actions) - 1)
+
+                self.board.move_with_index(next_action, p1)
+
+                print(self.board)
+
+                score = self.score()
+                if score != 0:
+                    break
+
+                if not self.board.has_move():
+                    break
+
+                actions = self.board.get_actions()
+                next_action = random.randint(0, len(actions) - 1)
+                self.board.move_with_index(actions[next_action], p2)
+                print(self.board)
+                score = self.score()
+
+            if score == -1:
+                print("Player 1 wins!")
+                wins += 1
+            elif score == 1:
+                print("Player 2 wins!")
+                losses += 1
+            else:
+                print("Draw.")
+                draws += 1
+            score = 0
+            self.board = Board()
+
+        print(f'wins: {wins} losses: {losses} draws: {draws}')
 
 class Player(object):
 
@@ -143,6 +226,9 @@ class Board(object):
         print(f'Move count: {self.__move_count}')
         return True
 
+    def move_with_index(self, i, player):
+        self.board_[i] = str(player)
+
     def has_move(self):
         return self.__move_count < 9
 
@@ -171,6 +257,14 @@ class Board(object):
             return self.board_[0] + self.board_[4] + self.board_[8]
         else:
             return self.board_[2] + self.board_[4] + self.board_[6]
+
+    def get_actions(self):
+        return [i for i, x in enumerate(self.board_) if x == self.EMPTY_SPACE]
+
+    def get_board_state_after_action(self, action, player):
+        local_board = self.board_.copy()
+        local_board[action] = str(player)
+        return ''.join(local_board)
 
     def generate_all_board_states(self, idx):
         """
@@ -202,6 +296,6 @@ class Board(object):
 if __name__ == "__main__":
     p1 = Player(1)
     p2 = Player(2)
-    game = TicTacToe(p1, p2)
-    game.initSimulation()
+    game = TicTacToe(p1, p2, alpha=.1)
+    game.simulate()
 
